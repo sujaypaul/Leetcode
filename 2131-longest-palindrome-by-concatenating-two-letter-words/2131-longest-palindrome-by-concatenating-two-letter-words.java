382f8f65cfe2effa092dda8cf89f2275548fb60c
Sujay Paul <58883319+sujaypaul@users.noreply.github.com>
2022-11-08 15:05:43 +0530

    Time: 29 ms (79.51%), Space: 95.9 MB (76.21%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,41 +1,23 @@
 class Solution {
     public int longestPalindrome(String[] words) {
-        Map<String, Integer> stringCount = new HashMap<>();
-        int res = 0;
-        int numSelfPalindrome = 0;
-        int n = words.length;
-        for (int i = 0; i < n ; i++) {
-            String s = words[i];
-            char a = s.charAt(0);
-            char b = s.charAt(1);
-            if (stringCount.containsKey(s)) {
-                // match found
-                res += 4;
-                stringCount.put(s, stringCount.get(s) - 1);
-                if (stringCount.get(s) == 0) {
-                    stringCount.remove(s);
-                }
-                if (a == b) {
-                    numSelfPalindrome--;
-                }
-            } else if (a == b) {
-                // same chars
-                stringCount.put(s, 1);
-                numSelfPalindrome++;
-            } else {
-                // reverse string and add to set
-                String toAdd = "" + b + a;
-                if (!stringCount.containsKey(toAdd)) {
-                    stringCount.put(toAdd, 1);
-                } else {
-                    stringCount.put(
-                        toAdd, stringCount.get(toAdd) + 1);
-                }
-            }
+        int ans = 0;
+        int[][] count = new int[26][26];
+
+      for (final String word : words) {
+          final int i = word.charAt(0) - 'a';
+          final int j = word.charAt(1) - 'a';
+          if (count[j][i] > 0) {
+            ans += 4;
+            --count[j][i];
+          } else {
+            ++count[i][j];
+          }
         }
-        if (numSelfPalindrome > 0) {
-            res += 2;
-        }
-        return res;
+
+        for (int i = 0; i < 26; ++i)
+          if (count[i][i] > 0)
+            return ans + 2;
+
+        return ans;
     }
 }
