d04a631229dec92a9a4f22ff6633fc9e46e9b836
Sujay Paul <58883319+sujaypaul@users.noreply.github.com>
2022-11-21 15:33:33 +0530

    Time: 14 ms (64.64%), Space: 67.5 MB (73.25%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -1,33 +1,37 @@
 class Solution {
-  public int nearestExit(char[][] maze, int[] entrance) {
-    final int m = maze.length;
-    final int n = maze[0].length;
-    final int[] dirs = {0, 1, 0, -1, 0};
-    int ans = 0;
-    Queue<int[]> q = new ArrayDeque<>(Arrays.asList(new int[] {entrance[0], entrance[1]}));
-    boolean[][] seen = new boolean[m][n];
-    seen[entrance[0]][entrance[1]] = true;
-
-    while (!q.isEmpty()) {
-      ++ans;
-      for (int sz = q.size(); sz > 0; --sz) {
-        final int i = q.peek()[0];
-        final int j = q.poll()[1];
-        for (int k = 0; k < 4; ++k) {
-          final int x = i + dirs[k];
-          final int y = j + dirs[k + 1];
-          if (x < 0 || x == m || y < 0 || y == n)
-            continue;
-          if (seen[x][y] || maze[x][y] == '+')
-            continue;
-          if (x == 0 || x == m - 1 || y == 0 || y == n - 1)
-            return ans;
-          q.offer(new int[] {x, y});
-          seen[x][y] = true;
+    public int nearestExit(char[][] maze, int[] entrance) {
+        int m = maze.length;
+        int n = maze[0].length;
+        
+        Queue<int[]> q = new LinkedList<>();
+        int ei = entrance[0];
+        int ej = entrance[1];
+        int[][] dvs = new int[][] {
+            {1, 0}, {0, 1}, {-1, 0}, {0, -1}    
+        };
+        
+        maze[ei][ej] = '+';
+        q.add(new int[] {ei, ej, 0});
+        
+        while(!q.isEmpty()) {
+            int[] val = q.poll();
+            
+            for (int[] d: dvs) {
+                int i1 = val[0] + d[0];
+                int j1 = val[1] + d[1];
+                
+                if (i1 < 0 || j1 < 0 || i1 >= m || j1 >= n) {
+                    if (val[0] == ei && val[1] == ej) continue;
+                    return val[2];
+                }
+                
+                if (maze[i1][j1] == '.') {
+                    q.add(new int[] {i1, j1, val[2] + 1});
+                    maze[i1][j1] = '+';
+                }
+            }
         }
-      }
+        
+        return -1;
     }
-
-    return -1;
-  }
 }
