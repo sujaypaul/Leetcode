33f35f9e86536cc1b402fc605dcda64c4ab83545
Sujay Paul <58883319+sujaypaul@users.noreply.github.com>
2022-12-19 06:33:13 +0530

    Time: 13 ms (98.34%), Space: 98.4 MB (97.53%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,31 +1,55 @@
-class Solution {
-    public boolean validPath(int n, int[][] edges, int start, int end) {
-        Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
-        for (int[] edge : edges) {
-            int node0 = edge[0], node1 = edge[1];
-            List<Integer> list0 = map.getOrDefault(node0, new ArrayList<Integer>());
-            List<Integer> list1 = map.getOrDefault(node1, new ArrayList<Integer>());
-            list0.add(node1);
-            list1.add(node0);
-            map.put(node0, list0);
-            map.put(node1, list1);
+class DisjointSetUnion{
+    private int[] parent;
+    private int[] rank;
+    private int N;
+    
+    public DisjointSetUnion(int n){
+        this.N = n;
+        this.parent = new int[this.N];
+        this.rank = new int[this.N];
+        for(int i = 0; i < this.N; i++){
+            this.parent[i] = i;
+            this.rank[i] = 1;
         }
-        boolean[] visited = new boolean[n];
-        Queue<Integer> queue = new LinkedList<Integer>();
-        visited[start] = true;
-        queue.offer(start);
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            if (node == end)
-                return true;
-            List<Integer> list = map.getOrDefault(node, new ArrayList<Integer>());
-            for (int next : list) {
-                if (!visited[next]) {
-                    visited[next] = true;
-                    queue.offer(next);
+    }
+    
+    public boolean areConnected(int u, int v){
+        return find(u) == find(v);
+    }
+    
+    public void union(int u, int v){
+        if(u != v){
+            int a = find(u);
+            int b = find(v);
+            if(a != b){
+                if(rank[a] > rank[b]){
+                    parent[b] = a;
+                    rank[a] += rank[b];
+                }else{
+                    parent[a] = b;
+                    rank[b] += rank[a];
                 }
             }
         }
-        return false;
+    }
+    
+    private int find(int u){
+        int x = u;
+        while(x != this.parent[x]){
+            x = this.parent[x];
+        }
+        
+        this.parent[u] = x;
+        return x;
+    }
+}
+class Solution {
+    public boolean validPath(int n, int[][] edges, int start, int end) {
+        DisjointSetUnion set = new DisjointSetUnion(n);
+        for(int[] edge : edges){
+            set.union(edge[0], edge[1]);
+        }
+        
+        return set.areConnected(start, end);
     }
 }
