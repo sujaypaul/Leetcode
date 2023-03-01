44d5ef8267db1b74580156de61ff066a3c7fd096
Sujay Paul <58883319+sujaypaul@users.noreply.github.com>
2023-03-01 13:09:03 +0530

    Time: 2141 ms (5.00%), Space: 56.1 MB (24.88%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,36 +1,28 @@
 class Solution {
   public int[] sortArray(int[] nums) {
-    heapSort(nums);
+    quickSort(nums, 0, nums.length - 1);
     return nums;
   }
 
-  private void heapSort(int[] A) {
-    buildMaxHeap(A);
-    int heapSize = A.length;
-    for (int i = A.length - 1; i > 0; --i) {
-      swap(A, i, 0);
-      --heapSize;
-      maxHeapify(A, 0, heapSize);
-    }
+  private void quickSort(int[] A, int l, int r) {
+    if (l >= r)
+      return;
+
+    final int m = partition(A, l, r);
+    quickSort(A, l, m - 1);
+    quickSort(A, m + 1, r);
   }
 
-  private void buildMaxHeap(int[] A) {
-    for (int i = A.length / 2; i >= 0; --i)
-      maxHeapify(A, i, A.length);
-  }
-
-  private void maxHeapify(int[] A, int i, int heapSize) {
-    final int l = 2 * i + 1;
-    final int r = 2 * i + 2;
-    int largest = i;
-    if (l < heapSize && A[largest] < A[l])
-      largest = l;
-    if (r < heapSize && A[largest] < A[r])
-      largest = r;
-    if (largest != i) {
-      swap(A, largest, i);
-      maxHeapify(A, largest, heapSize);
-    }
+  private int partition(int[] A, int l, int r) {
+    final int randIndex = new Random().nextInt(r - l + 1) + l;
+    swap(A, randIndex, r);
+    final int pivot = A[r];
+    int nextSwapped = l;
+    for (int i = l; i < r; ++i)
+      if (A[i] <= pivot)
+        swap(A, nextSwapped++, i);
+    swap(A, nextSwapped, r);
+    return nextSwapped;
   }
 
   private void swap(int[] A, int i, int j) {
