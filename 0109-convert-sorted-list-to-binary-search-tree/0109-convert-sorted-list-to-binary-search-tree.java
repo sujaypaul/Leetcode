287e4e07de38f0da9f83eac923e6c90bb8fba1ab
Sujay Paul <58883319+sujaypaul@users.noreply.github.com>
2023-03-11 10:11:03 +0530

    Time: 1 ms (40.56%), Space: 43.8 MB (36.94%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -1,55 +1,47 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
 class Solution {
   public TreeNode sortedListToBST(ListNode head) {
-    if (head == null)
-      return null;
-    if (head.next == null)
-      return new TreeNode(head.val);
+    List<Integer> A = new ArrayList<>();
 
-    ListNode mid = findMid(head);
-    TreeNode root = new TreeNode(mid.val);
-    root.left = sortedListToBST(head);
-    root.right = sortedListToBST(mid.next);
+    // Construct the array
+    for (ListNode curr = head; curr != null; curr = curr.next)
+      A.add(curr.val);
 
-    return root;
+    return helper(A, 0, A.size() - 1);
   }
 
-  private ListNode findMid(ListNode head) {
-    ListNode prev = null;
-    ListNode slow = head;
-    ListNode fast = head;
+  private TreeNode helper(List<Integer> A, int l, int r) {
+    if (l > r)
+      return null;
 
-    while (fast != null && fast.next != null) {
-      prev = slow;
-      slow = slow.next;
-      fast = fast.next.next;
-    }
-    prev.next = null;
-
-    return slow;
+    final int m = (l + r) / 2;
+    TreeNode root = new TreeNode(A.get(m));
+    root.left = helper(A, l, m - 1);
+    root.right = helper(A, m + 1, r);
+    return root;
   }
 }
