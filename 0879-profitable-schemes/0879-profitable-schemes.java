d1750eb165ecab2a49de4251c1f23fbd4c081ac2
Sujay Paul <58883319+sujaypaul@users.noreply.github.com>
2023-04-21 11:27:52 +0530

    Time: 26 ms (92.47%), Space: 40.9 MB (76.71%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.java
+++ 0879-profitable-schemes/0879-profitable-schemes.java
@@ -1,27 +1,22 @@
 class Solution {
   public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
     final int kMod = 1_000_000_007;
-    // dp[k][i][j] := # of schemes w/ first k crimes, AT MOST i members, and at
-    // Least j profits
-    int[][][] dp = new int[group.length + 1][n + 1][minProfit + 1];
+    // dp[i][j] := # of schemes w/ AT MOST i members and at least j profits
+    int[][] dp = new int[n + 1][minProfit + 1];
 
-    // No crimes, no profits, and any # of members
     for (int i = 0; i <= n; ++i)
-      dp[0][i][0] = 1;
+      dp[i][0] = 1;
 
     for (int k = 1; k <= group.length; ++k) {
       final int g = group[k - 1];
       final int p = profit[k - 1];
-      for (int i = 0; i <= n; ++i)
-        for (int j = 0; j <= minProfit; ++j)
-          if (i < g) {
-            dp[k][i][j] = dp[k - 1][i][j];
-          } else {
-            dp[k][i][j] = dp[k - 1][i][j] + dp[k - 1][i - g][Math.max(0, j - p)];
-            dp[k][i][j] %= kMod;
-          }
+      for (int i = n; i >= g; --i)
+        for (int j = minProfit; j >= 0; --j) {
+          dp[i][j] += dp[i - g][Math.max(0, j - p)];
+          dp[i][j] %= kMod;
+        }
     }
 
-    return dp[group.length][n][minProfit];
+    return dp[n][minProfit];
   }
 }
