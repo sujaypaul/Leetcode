40d04f90173e785781e9c058573b893e68e5b0fe
Sujay Paul <58883319+sujaypaul@users.noreply.github.com>
2023-05-19 17:08:24 +0530

    Time: 0 ms (100.00%), Space: 44.3 MB (6.63%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -5,28 +5,24 @@ class Solution {
     Color[] colors = new Color[graph.length];
     Arrays.fill(colors, Color.kWhite);
 
-    for (int i = 0; i < graph.length; ++i) {
-      // Already colored, do nothing
-      if (colors[i] != Color.kWhite)
-        continue;
-      // colors[i] == Color.kWhite
-      colors[i] = Color.kRed; // Always paint w/ Color.kRed
-      // BFS
-      Queue<Integer> q = new ArrayDeque<>(Arrays.asList(i));
-      while (!q.isEmpty()) {
-        for (int sz = q.size(); sz > 0; --sz) {
-          final int u = q.poll();
-          for (final int v : graph[u]) {
-            if (colors[v] == colors[u])
-              return false;
-            if (colors[v] == Color.kWhite) {
-              colors[v] = colors[u] == Color.kRed ? Color.kGreen : Color.kRed;
-              q.offer(v);
-            }
-          }
-        }
-      }
-    }
+    for (int i = 0; i < graph.length; ++i)
+      if (colors[i] == Color.kWhite && !isValidColor(graph, i, colors, Color.kRed))
+        return false;
+
+    return true;
+  }
+
+  private boolean isValidColor(int[][] graph, int u, Color[] colors, Color color) {
+    // The painted color should be same as the `color`
+    if (colors[u] != Color.kWhite)
+      return colors[u] == color;
+
+    colors[u] = color; // Always paint w/ `color`
+
+    // All children should have valid colors
+    for (final int v : graph[u])
+      if (!isValidColor(graph, v, colors, color == Color.kRed ? Color.kGreen : Color.kRed))
+        return false;
 
     return true;
   }
