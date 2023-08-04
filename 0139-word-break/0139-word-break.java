253ac1ef36c09237c3c6337f86173e0e57674588
Sujay Paul <58883319+sujaypaul@users.noreply.github.com>
2023-08-04 16:15:49 +0530

    Time: 7 ms (60.52%), Space: 43.7 MB (37.57%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,19 +1,20 @@
 class Solution {
   public boolean wordBreak(String s, List<String> wordDict) {
-    return wordBreak(s, 0, new HashSet<>(wordDict), new Boolean[s.length()]);
-  }
+    final int n = s.length();
+    Set<String> wordSet = new HashSet<>(wordDict);
+    // dp[i] := true if s[0..i) can be segmented
+    boolean[] dp = new boolean[n + 1];
+    dp[0] = true;
 
-  // Returns true if s[i:] can be segmented.
-  private boolean wordBreak(final String s, int i, Set<String> wordSet, Boolean[] memo) {
-    if (i == s.length())
-      return true;
-    if (memo[i] != null)
-      return memo[i];
+    for (int i = 1; i <= n; ++i)
+      for (int j = 0; j < i; ++j)
+        // s[0..j) can be segmented and s[j..i) in wordSet, so s[0..i) can be
+        // segmented
+        if (dp[j] && wordSet.contains(s.substring(j, i))) {
+          dp[i] = true;
+          break;
+        }
 
-    for (int j = i + 1; j <= s.length(); ++j)
-      if (wordSet.contains(s.substring(i, j)) && wordBreak(s, j, wordSet, memo))
-        return memo[i] = true;
-
-    return memo[i] = false;
+    return dp[n];
   }
 }
