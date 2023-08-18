8e1d599ce6fc25242412176d0afbe1dabd40ae52
Sujay Paul <58883319+sujaypaul@users.noreply.github.com>
2023-08-18 18:43:12 +0530

    Time: 4 ms (88.27%), Space: 44.9 MB (5.53%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -1,26 +1,20 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
-        Map<Integer, Set<Integer>> map = new HashMap<Integer, Set<Integer>>();
-        for (int[] road : roads) {
-            int city0 = road[0], city1 = road[1];
-            Set<Integer> set0 = map.getOrDefault(city0, new HashSet<Integer>());
-            Set<Integer> set1 = map.getOrDefault(city1, new HashSet<Integer>());
-            set0.add(city1);
-            set1.add(city0);
-            map.put(city0, set0);
-            map.put(city1, set1);
+        int[][] g = new int[n][n];
+        int[] cnt = new int[n];
+        for (var r : roads) {
+            int a = r[0], b = r[1];
+            g[a][b] = 1;
+            g[b][a] = 1;
+            ++cnt[a];
+            ++cnt[b];
         }
-        int maxRank = 0;
-        for (int i = 0; i < n; i++) {
-            Set<Integer> set0 = map.getOrDefault(i, new HashSet<Integer>());
-            for (int j = i + 1; j < n; j++) {
-                Set<Integer> set1 = map.getOrDefault(j, new HashSet<Integer>());
-                int rank = set0.size() + set1.size();
-                if (set0.contains(j))
-                    rank--;
-                maxRank = Math.max(maxRank, rank);
+        int ans = 0;
+        for (int a = 0; a < n; ++a) {
+            for (int b = a + 1; b < n; ++b) {
+                ans = Math.max(ans, cnt[a] + cnt[b] - g[a][b]);
             }
         }
-        return maxRank;
+        return ans;
     }
 }
